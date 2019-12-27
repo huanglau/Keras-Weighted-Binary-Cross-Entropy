3d734a57b422abfd9cfe1ae69c3f6ffb4d26e57f
laurie <lhuan68@uwo.ca>
2019-12-27 15:52:00 -0500

    built dyn cross entroyp

---
+++ DynCrossEntropy.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 28 15:09:26 2019
+
+@author: lhuang
+"""
+
+import keras
+
+def dyn_weighted_bincrossentropy(true, pred):
+    """
+    Calculates weighted binary cross entropy. The weights are determined dynamically
+    by the balance of each category. This weight is calculated for each batch.
+    
+    The weights are calculted by determining the number of 'pos' and 'neg' classes 
+    in the true labels, then dividing by the number of total predictions.
+    
+    For example if there is 1 pos class, and 99 neg class, then the weights are 1/100 and 99/100.
+    These weights can be applied so false negatives are weighted 99/100, while false postives are weighted
+    1/100. This prevents the classifier from labeling everything negative and getting 99% accuracy.
+    
+    This can be useful for unbalanced catagories.
+
+    """
+    # get the total number of inputs
+    num_pred = keras.backend.sum(keras.backend.cast(pred < 0.5, true.dtype)) + keras.backend.sum(true)
+    
+    # get weight of values in 'pos' category
+    zero_weight =  keras.backend.sum(true)/ num_pred +  keras.backend.epsilon() 
+    
+    # get weight of values in 'false' category
+    one_weight = keras.backend.sum(keras.backend.cast(pred < 0.5, true.dtype)) / num_pred +  keras.backend.epsilon()
+
+    # calculate the weight vector
+    weight_vector =  (1.0 - true) * zero_weight +  true * one_weight 
+    
+    # calculate the binary cross entropy
+    bin_crossentropy = keras.backend.binary_crossentropy(true, pred)
+    
+    # apply the weights
+    weighted_bin_crossentropy = weight_vector * bin_crossentropy 
+
+    return keras.backend.mean(weighted_bin_crossentropy)
